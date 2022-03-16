7f21178edf97cc9c4e4fa2d89f77bc4fe44e7018
Volandis <95025606+Volandis@users.noreply.github.com>
2022-03-16 09:48:27 +0000

    all updates from tue

---
+++ src/components/login/index.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { createUser, login } from "../../utils";
+
+export const Login = ({ setUser }) => {
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [pass, setPass] = useState();
+    const [bool, setBool] = useState(false);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        // setUser({ username: username, email: email, pass: pass });
+        if (bool) {
+            login(username, pass, setUser);
+        } else if (email && email.includes("@")) {
+            createUser(username, email, pass, setUser);
+        }
+    };
+
+    return (
+        <>
+            <form onSubmit={submitHandler}>
+            <input
+                onChange={(event) => setUsername(event.target.value)}
+                placeholder="Username"
+            />
+            {!bool && (
+                <input onChange={(event) => setEmail(event.target.value)}
+                placeholder="Email"
+                type="email"
+                />
+            )}
+            <input onChange={(event) => setPass(event.target.value)} placeholder="Password" type="password"/>
+            <button type="submit">Submit</button>
+            </form>
+            <button onClick={() => setBool(!bool)} >Log-in or Sign-up</button>
+        </>
+    );
+};
